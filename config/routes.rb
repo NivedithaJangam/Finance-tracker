b5c70892ca0e433b9d5406c53f5a1d7687b5cc08
NivedithaJangam <niveditha.jangam@gmail.com>
2020-03-10 14:54:53 +0530

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'friendships/create'
+  get 'friendships/destroy'
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
